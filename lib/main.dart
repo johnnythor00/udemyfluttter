9978235d86f4e48137a7e815d00cb0d5189096ff
johnnythor00 <irainocencio00@gmail.com>
2023-10-31 00:27:44 +0800

    Learning about colors

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+          backgroundColor: Color.fromARGB(255, 178, 58, 22),
+          body: Center(
+            child: Text('Hello World!'),
+          ),
+          ),
+    ),
+  );
+}
